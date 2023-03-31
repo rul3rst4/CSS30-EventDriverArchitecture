c49820d43e9776767794ed339c6bb0237bf67098
GedorN <gedor.neto@gmail.com>
2023-03-31 10:09:07 -0300

    Implementação inicial do producer

--- Producer/main.py
+++ Producer/main.py
@@ -1,1 +1,26 @@
-print("-_-")
+from kafka import KafkaProducer
+import requests
+import threading
+import time
+
+
+def bitcoin():
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    while True:
+        response = requests.get('https://www.mercadobitcoin.net/api/BTC/ticker/')
+        data = response.json()
+        producer.send('BITCOIN', bytes(data["ticker"]["buy"], encoding= 'utf-8'))
+        time.sleep(10)
+def dogecoin():
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    while True:
+        response = requests.get('https://www.mercadobitcoin.net/api/DOGE/ticker/')
+        data = response.json()
+        producer.send('DOGECOIN', bytes(data["ticker"]["buy"], encoding= 'utf-8'))
+        time.sleep(10)
+
+t = threading.Thread(target=bitcoin)
+t.start()
+
+t2 = threading.Thread(target=dogecoin)
+t2.start()
